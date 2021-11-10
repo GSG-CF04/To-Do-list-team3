f947fc2cbc0f32d20dfc40a2965cc728261a9971
IsraaHamdi <israaaburayya2012@gmail.com>
2021-11-10 18:48:54 +0200

    check function

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 let formInput = document.querySelector(".form-input"); //the add button
 let formBtn = document.querySelector(".form-btn"); //the input task
 let taskList = document.querySelector(".tasks-list"); //the whole list
-formBtn.addEventListener("click", (e) => {
-  e.preventDefault(); //to prevent the page from reloading
+
+let myTask ; //the task
+let tasks =[]; //array for all tasks
+formBtn.addEventListener("click",addList) 
+function addList() {
+  //e.preventDefault(); //to prevent the page from reloading
   if (formInput.value === "") return; // to prevent addding empty tasks
-  //ading the tasks in the list
-
-  taskList.insertAdjacentHTML(
-    "afterbegin",
-    `<li>
-  <button class="check"><i class="fas fa-check"></i></button>
-  <span>${formInput.value}</span>
-  <button class="edit"><i class="fas fa-pencil-alt"></i></button>
-  <button class="delete"><i class="fas fa-trash-alt"></i></button>
-</li>`
-  );
-
+  //define task
+  myTask = `<li id="${tasks.length}">
+  <span>${formInput.value} 
+  <button class="check" onclick="check(${tasks.length})"><i class="fas fa-check"></i></button>
+  <button class="edit" onclick="update(${tasks.length})"><i class="fas fa-pencil-alt"></i></button>
+  <button class="delete" onclick="deleteTask(${tasks.length})" ><i class="fas fa-trash-alt"></i></button>
+  </span>
+  </li>
+  `;
+  tasks.push(myTask);
+  //add task to whole list
+  taskList.insertAdjacentHTML("afterbegin",myTask);
   // local storage function place
 
   // clearing the input field
   formInput.value = "";
-});
+};
+
+function check(index){
+  //add checked class
+  document.getElementById(index).classList.add('checked')
+}
+function deleteTask(index){
+   document.getElementById(index).remove() 
+ }
