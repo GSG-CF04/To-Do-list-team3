0655116623ba5f2cdabb1ed217e2e4f12a62887f
Fady-Alwazir <fady.alwazir@gmail.com>
2021-11-10 13:17:23 +0200

    fixing the  submit problem relates #12

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let formInput = document.querySelector(".form-input"); //the add button
 let formBtn = document.querySelector(".form-btn"); //the input task
 let taskList = document.querySelector(".tasks-list"); //the whole list
-formBtn.addEventListener("click", (e) => {
+let form = document.querySelector(".submit-form");
+form.addEventListener("submit", (e) => {
   e.preventDefault(); //to prevent the page from reloading
   if (formInput.value === "") return; // to prevent addding empty tasks
   //ading the tasks in the list
